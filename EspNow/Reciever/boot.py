39a3fc9272c3a6be32557963076eac502d7e4302
javi <fordrsmax@gmail.com>
2024-11-14 13:22:11 +0100

    first tests on espnow
    led echo

--- EspNow/Reciever/boot.py
+++ EspNow/Reciever/boot.py
@@ -1,6 +1,19 @@
 import network
 import espnow
 
+from machine import Pin
+from utime import sleep
+
+led=Pin(2,Pin.OUT) # onboard led
+led.value(0)
+
+def flash(speed):
+    for i in range(3):
+        led.value(1)
+        sleep(speed)
+        led.value(0)
+        sleep(speed)
+
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
@@ -10,12 +23,10 @@ e = espnow.ESPNow()
 e.active(True)
 
 while True:
-    try:
-        host, msg = e.recv()
-        if msg: # msg == None if timeout in recv()
+    host, msg = e.recv()
+    if msg: # msg == None if timeout in recv()
+        if msg == b'lap':
             db = e.peers_table[host][0]
             # print(host,msg)
             print(db)
-    except:
-        break
-        
+            flash(0.1)
--- EspNow/Sender/boot.py
+++ EspNow/Sender/boot.py
@@ -1,6 +1,18 @@
 import network
 import espnow
-import utime
+
+from machine import Pin
+from utime import sleep
+
+led=Pin(2,Pin.OUT) # onboard led
+led.value(0)
+
+def flash(speed):
+    for i in range(3):
+        led.value(1)
+        sleep(speed)
+        led.value(0)
+        sleep(speed)
 
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)  # Or network.AP_IF
@@ -12,12 +24,17 @@ e.active(True)
 peer = b'\xbb\xbb\xbb\xbb\xbb\xbb'   # MAC address of multicast 
 e.add_peer(peer)      # Must add_peer() before send()
 
+print("espnow peer conection...")
+
 e.send(peer, "Starting...")
+
+print("starting to send lap ping")
 
 while True:
     try:
-        e.send(peer,"lap")
-        utime.sleep_ms(1000)
+        response = e.send(peer,"lap")
+        flash(0.1)
+        sleep(1)
         pass
     except:
         break
